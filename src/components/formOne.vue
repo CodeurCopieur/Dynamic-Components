0714e88080369cc6fba0648583c32ade71d84b99
codwerk <louis_widdy@live.fr>
2020-06-16 13:49:26 +0200

    component formOne(login) and formTwo(contact)

---
+++ src/components/formOne.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>FormOne</h1>
+    <form action="">
+      <div class="form-header">
+        <h3>Form One - Login </h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur.
+        </p>
+      </div>
+
+      <div class="form-fields">
+        <input type="text" placeholder="name">
+        <input type="password" placeholder="password">
+      </div>
+
+      <div class="form-controls">
+        <button >Login</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/formTwo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>FormTwo</h1>
+     <form action="">
+      <div class="form-header">
+        <h3>Form One - Login </h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur.
+        </p>
+      </div>
+
+      <div class="form-fields">
+        <input type="text" placeholder="name">
+        <textarea name="" id="" cols="10" rows="10"></textarea>
+      </div>
+
+      <div class="form-controls">
+        <button >Send</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
